7a23015ebc7a091fb1318736705c3046d5bea6f3
juliorcx <julio.rc.design@gmail.com>
2019-04-28 20:45:47 +0200

    DWM with 'Fn' key enabled

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -63,6 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 
+/* Here are the volume and brightness control with 'Fn key', and also screenshot */
 /* audio */
 static const char *volup[]         = { "amixer", "set", "Master", "2+",     NULL };
 static const char *voldown[]       = { "amixer", "set", "Master", "2-",     NULL };
